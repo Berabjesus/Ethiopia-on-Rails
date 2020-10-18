64020408a0dcd6d5c2389edbf41a37fdd27d7f84
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-18 19:33:23 +0300

    ADD test button added fro google oauth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def oauth
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # get 'sessions/index'
+  # get 'sessions/show'
   # resources :users
   
-  # root to: 'users#index'
+  # get 'auth/google_oauth2/callback', to 'sessions#oauth'
+  # get '/auth/github/callback', to 'sessions#oauth'
+  get '/auth/:provider/callback' => "sessions#oauth"
+
+  root to: 'sessions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
