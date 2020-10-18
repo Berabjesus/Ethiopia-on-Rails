99713537c4e67bce88308964009dbf586f6bf9fc
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-19 00:55:20 +0300

    ADD omniauth sample login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,14 +10,20 @@ class SessionsController < ApplicationController
   def oauth
     # redirect_to sessions_path
     user = User.find_or_create_by(oauthid: oauth_params['uid'], provider: oauth_params['provider']) do |u|
-      u.name = oauth_params['info']['name']
-      u.password = SecureRandom.hex(16)
+      # u.name = oauth_params['info']['name']
+      u.name = "Bereket Ababu"
+      # u.password = SecureRandom.hex(16)
     end
-
     if user.valid?
-      # @auth= user
       session[session_name] = user.id
       redirect_to articles_path
+    else
+      errors = 'Authentication Error! <br>'
+      errors << user.errors.full_messages.join(', ').gsub(",", "<br>")
+      errors << "<br> Please signup / login using your username and password if this persists"
+      respond_to do |format|
+        format.html { redirect_to new_user_path, alert: errors.html_safe }
+      end
     end
   end
 
