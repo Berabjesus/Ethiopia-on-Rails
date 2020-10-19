9625446e062b799498bd21ef1094aa732a30b892
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-19 15:22:43 +0300

    ADD test index field for the logged in user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,30 @@
 class ArticlesController < ApplicationController
   before_action :logged_in?
-  
+
   def index
   end
 
   def new
+    @article = Article.new
   end
 
   def show
+  end
+
+  def create
+    # @article = Article.new(article_params)
+    # @article.AuthorId = current_user.id
+    @article = current_user.articles.build(article_params)
+    if @article.valid? && @article.save
+      redirect_to articles_path, notice: 'article Created Successfully'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text, :image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, uniqueness: true, presence: true
+  has_many :articles, :foreign_key => "AuthorId", :class_name => "Article",  dependent: :destroy
 end
