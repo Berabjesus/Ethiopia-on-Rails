dfa0c138b857d2c119a84196b14416886da78fc5
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-19 17:56:05 +0300

    UPDATE omniauth.rb file with proper client_ids

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,6 @@
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.google['google_client_id'], Rails.application.credentials.google['google_client_secret']
-  provider :github, Rails.application.credentials.github['github_client_id'], Rails.application.credentials.github['github_client_secret']
+  provider :google_oauth2, Rails.application.credentials.google[:google_client_id], Rails.application.credentials.google[:google_client_secret]
+  provider :github, Rails.application.credentials.github[:github_client_id], Rails.application.credentials.github[:github_client_secret]
 end
