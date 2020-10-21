79341507ae35e278d4b70101a58bb2153ac84532
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-21 22:14:14 +0300

    ADD header navigation links logic added in the application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  def navigation_buttons
+      link = capture {link_to 'Home', articles_path, class: 'text-uppercase border-dark pr-3'}
+      link << capture {link_to 'History', articles_path, class: 'text-uppercase border-dark pr-3'}
+      link << capture {link_to 'Geography', articles_path, class: 'text-uppercase border-dark pr-3'}
+      link << capture {link_to 'Politics', articles_path, class: 'text-uppercase border-dark pr-3'}
+      link << capture {link_to 'Sports', articles_path, class: 'text-uppercase border-dark pr-3'}
+      link << capture {link_to 'Others', articles_path, class: 'text-uppercase border-dark pr-3'}
+      link << content_tag(:ins, "|") if logged_in?
+      link << capture {link_to 'Create new article', articles_path, class: 'text-uppercase border-dark pl-2'} if logged_in?
+      link
+  end
+
   def access_buttons
     if logged_in? 
       link = capture {link_to (image_tag('', class: 'fa fa-user pr-2')), users_path}
