838df6788659aa6e382f8e9504007ba03ba409a5
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-21 20:57:57 +0300

    ADD user sign up logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.permit(:name, :password, :password_confirmation)
   end
   
 end
