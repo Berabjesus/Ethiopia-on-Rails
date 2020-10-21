854985c3cd28e96800602288de4900177f67361c
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-21 16:48:31 +0300

    ADD search modal to the header in the applciation.html.erb file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,11 @@ module ApplicationHelper
       # link = capture {link_to current_user.name, class: 'text-uppercase border-dark pr-2'}
       link = capture {link_to (image_tag('', class: 'fa fa-user pr-2')), users_path}
       link << content_tag(:ins, "|")
-      link << capture {link_to 'Sign Out', articles_path, class: 'text-uppercase border-dark ml-1'}
+      link << capture {link_to 'Sign Out', articles_path, class: 'text-uppercase border-dark ml-2'}
     else
       link = capture {link_to 'Login', articles_path, class: 'text-uppercase border-dark pr-2'}
       link << content_tag(:ins, "|")
       link << capture {link_to 'Register', articles_path, class: 'ml-1 text-uppercase'}
-
     end
     link
   end
