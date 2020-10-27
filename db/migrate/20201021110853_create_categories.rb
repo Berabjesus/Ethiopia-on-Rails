acec5dc67894679519a14b3f0920a316d26e2251
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-27 15:02:09 +0300

    UPDATE category migration with default categories

--- db/migrate/20201021110853_create_categories.rb
+++ db/migrate/20201021110853_create_categories.rb
@@ -6,5 +6,13 @@ class CreateCategories < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
+
+    categories = %w[Others Sports Politics Geography History]
+    categories.each_with_index do |category_name, index|
+      category = Category.new
+      category.name = category_name
+      category.priority = index
+      category.save
+    end
   end
 end
